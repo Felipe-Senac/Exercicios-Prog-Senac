30378241db43c0fe9888b1fba0be9da9e3f20c0b
Felipe Varanda <varandafelipe2008@gmail.com>
2024-11-19 17:19:16 -0300

    PRIMEIRA LISTA

---
+++ LISTA 1/LISTA E-1/Exemplo 3.js
@@ -0,0 +1,3 @@
+var A = Number(prompt("Enter the first number for the operation:"));
+var B = Number(prompt("Enter the second number for the operation"));
+document.write(`A soma de ${A} e ${B} é igual a ${A + B}`);
