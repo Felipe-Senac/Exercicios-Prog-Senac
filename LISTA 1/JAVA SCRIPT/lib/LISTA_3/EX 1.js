a16f570e70297b3665af1d68415120599d9d61b7
Felipe Varanda <varandafelipe2008@gmail.com>
2024-11-19 17:11:37 -0300

    Lista 3

---
+++ LISTA 1/JAVA SCRIPT/lib/LISTA_3/EX 1.js
@@ -0,0 +1,10 @@
+function avaliar(media) {
+if (media >= 7.0){return "Aprovado"}
+else{return "Reprovado"}
+}
+console.log(avaliar(10))
+console.log(avaliar(7.0))
+console.log(avaliar(6.95))
+console.log(avaliar(8.8))
+console.log(avaliar(0))
+console.log(avaliar(2.725))
