e20cd10971af89501e1d6b355abd2f98fae31b70
Felipe Varanda <varandafelipe2008@gmail.com>
2024-11-19 17:29:18 -0300

    Lista 3

---
+++ LISTA 1/JAVA SCRIPT/lib/LISTA_3/EX 3.js
@@ -0,0 +1,10 @@
+function PouN(sinal){
+if (sinal > 0){return "Positivo"}
+else if (sinal < 0 ){return "Negativo"}
+else {return "Zero"}
+}
+console.log(PouN(5))
+console.log(PouN(-3))
+console.log(PouN(0))
+console.log(PouN(1110))
+console.log(PouN(-987654321))
