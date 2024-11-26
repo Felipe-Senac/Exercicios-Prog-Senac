18792e68611bd83c5e5f9e1082daa88e4662b1fa
Felipe Varanda <varandafelipe2008@gmail.com>
2024-11-26 17:24:52 -0300

    CARRO

---
+++ LISTA 1/CARRO.js
@@ -0,0 +1,19 @@
+var carro = {
+marca: "Bugatti",
+modelo: "Chiron",
+ano: 2025,
+cor: "Preta",
+potência: 1500,
+velocidade_maxima: "420Km/h",
+motor: "W16 8.0 quadriturbo",
+combustão: "Gasolina",
+portas: 2,
+carroceria: "Coupe",
+cambio: "automatico",
+nacionalidade: "França",
+tração: "integral",
+classe: "superesportivo",
+calibragem: { dianteiro: 32, traseiro: 32, step: 36}
+}
+console.log(carro)
+console.table(carro)
