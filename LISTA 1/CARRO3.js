aa628b88437a84eb9e18477b2ec3c28121180f09
Felipe Varanda <varandafelipe2008@gmail.com>
2024-11-28 17:19:40 -0300

    CARRO

--- LISTA 1/CARRO3.js
+++ LISTA 1/CARRO3.js
@@ -1,25 +1,9 @@
 var carro = {
  modelo:"Fusca",
  cor: "Preto",
- velocidade: 0}
- function acelerar() {
-    carro.velocidade += 5
-        }
-     while(carro.velocidade < 80) acelerar()
-     function desacelerar() {
-      carro.velocidade -= 13
-    if(carro.velocidade < 0) {
-        carro.velocidade = 0
-    }
-    }
-    while (carro.velocidade > 0)desacelerar()
-    console.log(carro)
-    
-    
-    
-    
-    
-    
-    
-    
+ velocidade: 0,
+acelerar: function() { this.velocidade += 15;},
+imprimir: function() { console.table(this);},
+}  
+       
     
