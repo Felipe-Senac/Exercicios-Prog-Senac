473bac07b3e863fd3ceec96245dc7acd791db1ac
Felipe Varanda <varandafelipe2008@gmail.com>
2024-12-06 14:26:30 -0300

    LISTA 4

---
+++ LISTA 1/LISTA 4/E-3.js
@@ -0,0 +1,6 @@
+var arr1 = [ 1, 2, 3, 4, 5];
+var arr2 = [ 10, 20, 30, 40, 50];
+var arr3 = [ 100, 200, 300];
+console.log(arr1.indexOf(3) != -1);
+console.log(arr2.indexOf(25) != -1);
+console.log(arr3.indexOf(100) != -1);
