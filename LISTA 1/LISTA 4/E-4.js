eb63ae50a95c61df250b595c2dc43355af05187b
Felipe Varanda <varandafelipe2008@gmail.com>
2024-12-06 15:03:29 -0300

    Lista 4

---
+++ LISTA 1/LISTA 4/E-4.js
@@ -0,0 +1,12 @@
+var arr1 = [1, 2, 3, 4, 5];
+var arr2 = ["a", "b", "c"];
+var arr3 = [10];
+var arr4 = [];
+arr1.pop()
+arr2.pop()
+arr3.pop()
+arr4.pop()
+console.log(arr1)
+console.log(arr2)
+console.log(arr3)
+console.log(arr4)
