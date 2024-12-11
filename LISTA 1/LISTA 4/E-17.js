0299152966b978e3dd3034de392f406950c0ddfb
Felipe Varanda <varandafelipe2008@gmail.com>
2024-12-11 17:24:06 -0300

    Lista 4

---
+++ LISTA 1/LISTA 4/E-17.js
@@ -0,0 +1,4 @@
+function verify(x){if(isNaN(x)){return false}else{return true}}
+console.log(verify("12345"));
+console.log(verify("abc123"));
+console.log(verify("67890"));
---
+++ LISTA 1/LISTA 4/mediatest.js
@@ -0,0 +1,12 @@
+// media
+//const
+
+
+
+
+
+
+// lista de numeros "5 7 90 100 45"
+var strin = "5 7 90 100 45"
+var spl = (strin.split(' '));
+console.log(spl.map(Number))
