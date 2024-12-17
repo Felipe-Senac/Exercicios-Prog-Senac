f8c0ac7fd625a0bf7db085689f188ec26575a2af
Felipe Varanda <varandafelipe2008@gmail.com>
2024-12-17 16:53:46 -0300

    lista final

--- LISTA 1/LISTA 4/E-10.js
+++ LISTA 1/LISTA 4/E-10.js
@@ -1,12 +1,4 @@
-function soma(array){
-    if (array.length === 0){return 0;}
-    let conta = 0;
-    for(let index = 0; index < array.length; index ++ ){
-    if(array[index] % 2 === 0){conta +=array[index];}
-    }
-    
-    return conta;
-    }
-    console.log(soma([1, 2, 3, 4, 5]));
-    console.log(soma([2, 4, 6, 8]));
-    console.log(soma([1, 3, 5, 7]));
+import { somapar } from "../biblioteca4.js"
+    console.log(somapar([1, 2, 3, 4, 5]));
+    console.log(somapar([2, 4, 6, 8]));
+    console.log(somapar([1, 3, 5, 7]));
--- LISTA 1/LISTA 4/E-9.js
+++ LISTA 1/LISTA 4/E-9.js
@@ -1,11 +1,4 @@
-function soma(array){
-if (array.length === 0){return 0;}
-let conta = 0;
-for(let index = 0; index < array.length; index ++ ){
- conta +=array[index];
-}
-return conta;
-}
+import { soma } from "../biblioteca4.js"
 console.log(soma([1,2,3,4,5]));
 console.log(soma([2, 4, 6, 8]));
 console.log(soma([1, 3, 5, 7]));
